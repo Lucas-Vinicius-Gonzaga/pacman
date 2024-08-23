45a658d82600fac7a7147ac1a3525c92e3150fcc
Lucas-Vinicius-Gonzaga <lucasviniciusgonzaga1234@gmail.com>
2024-08-23 09:01:10 -0300

    mudei a quantidade de vida

--- jogo.js
+++ jogo.js
@@ -12,7 +12,7 @@ const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
-let lives = 3;
+let lives = 7;
 let ghostCount = 2;
 let ghostImageLocations = [
     { x: 0, y: 0 },
